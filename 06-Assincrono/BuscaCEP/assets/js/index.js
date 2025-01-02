fcd82b3928b89fd3c52dff7889591fd00fa455bb
Gabriel De Liz <gabriellizcfnpbnu@gmail.com>
2025-01-02 17:07:39 -0300

    arruamndo testes

--- 06-Assincrono/BuscaCEP/assets/js/index.js
+++ 06-Assincrono/BuscaCEP/assets/js/index.js
@@ -1,31 +1,34 @@
-const btnEnviar = document.getElementById("btn-enviar")
-
-btnEnviar.addEventListener('click', async e => {
-    e.preventDefault()
-    const input = document.getElementById('search-cep')
-    const cepFormat = input.value.replace(/\D/g, '')
-    const cep = await fetchCep(cepFormat)
-    if (cep) loadData(cep)
-})
+const btnEnviar = document.getElementById('btn-enviar');
+const opa = 1 + 2 / 3;
+console.log(opa);
+btnEnviar.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const input = document.getElementById('search-cep');
+    const cepFormat = input.value.replace(/\D/g, '');
+    const cep = await fetchCep(cepFormat);
+    console.log(cep);
+    if (cep) loadData(cep);
+});
 
 async function fetchCep(cep) {
     try {
-        const url = `https://viacep.com.br/ws/${cep}/json/`
-        const response = await fetch(url)
-        if (response.status !== 200) throw new Error("Error in search cep") 
-        const json = await response.json()
-        return json
-    } catch(err) {
-        console.log(err)
+        const url = `https://viacep.com.br/ws/${cep}/json/`;
+        const response = await fetch(url);
+        if (response.status !== 200) throw new Error('Error in search cep');
+        const json = await response.json();
+        return json;
+    } catch (err) {
+        console.log(err);
     }
 }
 
 function loadData(cep) {
-    const container = document.getElementById("resultado")
-    container.textContent = ""
-    const p = document.createElement("p")
+    const container = document.getElementById('resultado');
+    container.textContent = '';
+    const p = document.createElement('p');
     p.textContent = `
         ${cep.logradouro}, ${cep.bairro} - ${cep.localidade} | ${cep.estado} (${cep.uf})
-    `
-    container.appendChild(p)
+    `;
+    container.appendChild(p);
 }
+
